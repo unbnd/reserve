5aa1779fa8376f0a282a8d7a62885568833dd643
stellastella <founder@petaltech.xyz>
2024-08-21 15:35:31 +0300

    im having first hand embarassment

--- assets/js/supabase.js
+++ assets/js/supabase.js
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-const supabase = createClient('[https://xyzcompany.supabase.co](https://ividghsmcwfjxsgumkny.supabase.co/)', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2aWRnaHNtY3dmanhzZ3Vta255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxNzMwNTYsImV4cCI6MjAzOTc0OTA1Nn0.2EjsXqZGbDkdPJ5OCT5E7FhXThvLglsuji78DIG2t0w')
+const supabase = createClient('https://ividghsmcwfjxsgumkny.supabase.co/)', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml2aWRnaHNtY3dmanhzZ3Vta255Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjQxNzMwNTYsImV4cCI6MjAzOTc0OTA1Nn0.2EjsXqZGbDkdPJ5OCT5E7FhXThvLglsuji78DIG2t0w')
